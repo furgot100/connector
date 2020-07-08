a700f7801e58b881d9b725bc5839eea6f79415ac
Francis Tsang <frtsang40@gmail.com>
2020-07-07 20:07:41 -0700

    create route working

--- app.js
+++ app.js
@@ -2,20 +2,27 @@
 const express = require('express')
 const app = express()
 
-// require handlebars
-const exphbs = require('express-handlebars');
 // INITIALIZE BODY-PARSER AND ADD IT TO APP
 const bodyParser = require('body-parser');
 
+const handlebars = require('handlebars');
+const { allowInsecurePrototypeAccess } = require('@handlebars/allow-prototype-access');
+
+const exphbs = require('express-handlebars')
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    handlebars: allowInsecurePrototypeAccess(handlebars),
+});
+
 // Use "main" as our default layout
-app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+app.engine('handlebars', hbs.engine);
 // Use handlebars to render
 app.set('view engine', 'handlebars');
-
+// The following line must appear AFTER const app = express() and before your routes!
 app.use(bodyParser.urlencoded({ extended: true }));
+const models = require('./db/models');
 
-
-var events = [
+var lobbys = [
     { title: "I am your first event", desc: "A great event that is super fun to look at and good"},
     { title: "I am your second event", desc: "A great event that is super fun to look at and good"},
     { title: "I am your third event", desc: "A great event that is super fun to look at and good"}
@@ -23,7 +30,18 @@ var events = [
 
 // INDEX
 app.get('/', (req, res) => {
-    res.render('home', { events: events });
+    models.Lobby.findAll().then(lobbys => {
+      res.render('home', { lobbys: lobbys });
+    })
+  })
+
+// CREATE
+app.post('/lobby', (req, res) => {
+    models.Lobby.create(req.body).then(lobby => {
+      res.redirect(`/`);
+    }).catch((err) => {
+      console.log(err)
+    });
 })
 
 // NEW
