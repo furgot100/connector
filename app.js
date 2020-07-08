98f74317545944a5234dca180201bfd4143c5cee
Francis Tsang <frtsang40@gmail.com>
2020-07-07 18:04:27 -0700

    project init and setup

---
+++ app.js
@@ -0,0 +1,30 @@
+// Initialize express
+const express = require('express')
+const app = express()
+
+// require handlebars
+const exphbs = require('express-handlebars');
+
+// Use "main" as our default layout
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
+// Use handlebars to render
+app.set('view engine', 'handlebars');
+
+var events = [
+    { title: "I am your first event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+    { title: "I am your second event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" },
+    { title: "I am your third event", desc: "A great event that is super fun to look at and good", imgUrl: "https://img.purch.com/w/660/aHR0cDovL3d3dy5saXZlc2NpZW5jZS5jb20vaW1hZ2VzL2kvMDAwLzA4OC85MTEvb3JpZ2luYWwvZ29sZGVuLXJldHJpZXZlci1wdXBweS5qcGVn" }
+]
+
+// INDEX
+app.get('/', (req, res) => {
+    res.render('home', { events: events });
+  })
+
+// Choose a port to listen on
+const port = process.env.PORT || 3000;
+
+// Tell the app what port to listen on
+app.listen(port, () => {
+  console.log('App listening on port 3000!')
+})
