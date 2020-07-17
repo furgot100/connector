87e3f4cf617567bfa021f63b94a5b928b7b93013
Francis Tsang <frtsang40@gmail.com>
2020-07-17 15:39:02 -0700

    adding sql

--- db/migrations/20200709174837-add-lobby-id-to-comments.js
+++ db/migrations/20200709174837-add-lobby-id-to-comments.js
@@ -3,22 +3,11 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
 
-    return queryInterface.addColumn('Comments', 'LobbyId', Sequelize.INTEGER).then(() => {
-      return queryInterface.addConstraint('Comments', ['LobbyId'], {
-        type: 'foreign key',
-        name: 'lobby_comments',
-        references: { //Required field
-          table: 'Lobbies',
-          field: 'id'
-        },
-        onDelete: 'CASCADE',
-        onUpdate: 'CASCADE'
-      });
-    });
+    
   },
 
   down: (queryInterface, Sequelize) => {
-    return queryInterface.removeColumn('Comments', 'LobbyId');  
+
   }
 };
 
