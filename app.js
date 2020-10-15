9500c6154145a5c53f9ddd185c80ef0ca9a465f7
Francis Tsang <frtsang40@gmail.com>
2020-10-15 13:11:59 -0700

    added user table and middleware to app.js

--- app.js
+++ app.js
@@ -3,6 +3,41 @@ const express = require('express')
 const methodOverride = require('method-override')
 const app = express()
 
+// PASSPORT NEED MODULES
+const crypto = require('crypto');
+const mysql = require('mysql')
+const session = require("express-session");
+const MySQLStore = require('express-mysql-session')(session);
+const passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
+// MYSQL connection and instantiate the session store
+var connection = mysql.createConnection({
+  host: process.env.SESSIONSDB_HOST,
+  port: process.env.SESSIONSDB_PORT,
+  user: process.env.SESSIONSDB_USER,
+  password: process.env.SESSIONSDB_PASS,
+  database: process.env.SESSIONSDB_DB
+});
+
+var sessionStore = new MySQLStore({
+  checkExpirationInterval: parseInt(process.env.SESSIONSDB_CHECK_EXP_INTERVAL, 10),
+  expiration: parseInt(process.env.SESSIONSDB_EXPIRATION, 10)
+}, connection);
+
+// Session middleware 
+/* Create a cookie that expires in 1 day */
+var expireDate = new Date();
+expireDate.setDate(expireDate.getDate() + 1);
+
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: process.env.SESSIONSDB_SECRET,
+  store: sessionStore,
+  cookie: { expires: expireDate }
+}));
+
 
 // INITIALIZE BODY-PARSER AND ADD IT TO APP
 const bodyParser = require('body-parser');
@@ -28,7 +63,10 @@ app.use(methodOverride('_method'))
 
 app.use(express.static('public'));
 
-require('./controllers/auth.js')(app);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// require('./controllers/auth.js')(app);
 require('./controllers/lobby')(app, models);
 require('./controllers/comment')(app, models);
 
